383d74ffce443eecf41cd8c85d1465086c40294d
erk.cgn <erikac.ec80@gmail.com>
2023-10-06 14:41:29 -0300

    creo fn cotizar el seguro y muestro en appSeguro

--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState, createContext} from 'react'
+import { obtenerDiferenciaYear, calcularMarca, calcularPlan, formatearDinero} from '../helpers'
 
 const CotizadorContext = createContext()
 
@@ -12,6 +13,7 @@ const CotizadorProvider = ({children}) => {
     })
 
     const [error, setError] = useState('')
+    const [resultado, setResultado] = useState(0)
 
     const handleChangeDatos = e => {
         setDatos({
@@ -22,17 +24,26 @@ const CotizadorProvider = ({children}) => {
     
     const cotizarSeguro = () => {
         //una base
+        let resultado = 2000
 
         //obtener diferencia de años
-
+        const diferencia = obtenerDiferenciaYear(datos.year)
+        
         //hay que restar el 3% x c/año
-
+        resultado -= ((diferencia * 3) * resultado ) / 100
+        
         //americano 15%
         //europeo 30%
         //asiatico 5%
-
+        resultado *= calcularMarca(datos.marca)
         //basico 20%
         //completo 50%
+        resultado *= calcularPlan(datos.plan)
+
+        //Formatear dinero        
+        resultado = formatearDinero(resultado)
+
+        setResultado(resultado)
     }
 
     return(
@@ -42,7 +53,8 @@ const CotizadorProvider = ({children}) => {
                 handleChangeDatos,
                 error,
                 setError,
-                cotizarSeguro
+                cotizarSeguro,
+                resultado
             }}
         >
             {children}
