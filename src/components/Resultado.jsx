1743c302377e98773d38269f983aa573625c7725
erk.cgn <erikac.ec80@gmail.com>
2023-10-09 10:36:38 -0300

    useCallback y useRef para re render

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useMemo, useRef} from 'react'
 import useCotizador from "../hooks/useCotizador"
 import { MARCAS, PLANES} from '../constants'
 
@@ -5,10 +6,16 @@ import { MARCAS, PLANES} from '../constants'
 const Resultado = () => {
     const { resultado, datos } = useCotizador()
     const { marca, plan , year } = datos
+    const yearRef = useRef(year)
 
-    const [nombreMarca] = MARCAS.filter(m  => m.id === Number(marca))
-    const [nombrePlan] = PLANES.filter(p  => p.id === Number(plan))
-    
+    const [nombreMarca] = useCallback( 
+        MARCAS.filter(m  => m.id === Number(marca)),
+        [resultado]
+    ) 
+    const [nombrePlan] = useCallback(
+        PLANES.filter(p  => p.id === Number(plan)),
+        [resultado]
+    )    
 
     if(resultado === 0) return null;
 
@@ -27,7 +34,7 @@ const Resultado = () => {
         </p>
         <p className="my-2">
             <span className="font-bold">Año del Auto: </span>
-            {year}
+            {yearRef.current}
         </p>
         <p className="my-2 text-2xl">
             <span className="font-bold">Total Cotización: </span>
